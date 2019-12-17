4112892affca4cae1a7fb45746dad931c270dd41
magendiran1 <magendiranmendy@gmail.com>
2019-12-17 22:31:37 +0530

    HHTP adding

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import Hai1 from './Component/Propschildren/children';
 // import Clickcounter from './Component/Conditional/RenderProsp/Clickcounter';
 import Fragment from './Component/Fragment';
 import Demo from './Component/MultipleClass/demo';
-import Count from  './Context/Count'
+import Count from  './Context/count'
+import Getmethod from './Component/HTTP/HTTps'
 // import learn from './Component/Lifes/Learn';
 // import FunctionClick from './Component/Functionclick';
 // import Clickinclass from './Component/ClassClick';
@@ -58,6 +59,7 @@ function App() {
 <Hai1 />
 <Demo />
 <Count />
+<Getmethod />
 </div>
   );
 }
---
+++ src/Component/HTTP/HTTps.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axois from 'axios'
+class Getmethod extends Component {
+    constructor(props) {
+        super(props);
+        
+    this.state ={
+        Post :[]
+    }
+    }
+    componentDidMount(){
+        axois.get('http://jsonplaceholder.typicode.com/posts')
+        .then(Response=>{
+            console.log(Response)
+            this.setState({Post:Response.data})
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    }
+
+    render() {
+        const {Post}=this.state
+        return (
+            <div>
+                <h2>post method </h2>
+                {
+                    Post.length ?
+                    Post.map(Post => <div key={Post.id}>{Post.title}</div>) :
+                    null
+                }
+            </div>
+        );
+    }
+}
+
+export default Getmethod;
