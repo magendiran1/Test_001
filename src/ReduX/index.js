7a33ebb8ce56ef084bc7ec6625107ecaeaaa1db2
magendiran1 <magendiranmendy@gmail.com>
2020-01-15 09:40:28 +0530

    Redux

--- src/ReduX/index.js
+++ src/ReduX/index.js
@@ -1,1 +1,9 @@
 console.log("Hi magendiran")
+
+// Action
+// 1.Action  is the  only way  your  application can interact with the store(manage the state)
+// 2.Action carry some Information from your application to Redux store
+// 3. Action is plain javascript object
+// 
+// 4. Action have "type" that indicates the type of action  being performed
+// 5. The "Type" property is typically defined by string constants
