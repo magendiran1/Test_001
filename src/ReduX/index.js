1b575457da23f37c1f384ebe7e5e259f029146cf
magendiran1 <magendiranmendy@gmail.com>
2020-01-16 08:48:08 +0530

    Redux

--- src/ReduX/index.js
+++ src/ReduX/index.js
@@ -7,3 +7,11 @@ console.log("Hi magendiran")
 // 
 // 4. Action have "type" that indicates the type of action  being performed
 // 5. The "Type" property is typically defined by string constants
+
+
+// Store
+// 1.  one Store for Entire Application 
+// 2.Allow  access to State via Getstate()
+// 3.Allow update  to be updated via dispatch(action)
+// 4.Register listeners via subscribe 
+// 5. Handles Unregistering of listener via function returned by subscribe 
